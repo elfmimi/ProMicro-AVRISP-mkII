0f15fa58ee3a40369a17ba2969024517aa7a05d2
Drashna Jael're <drashna@live.com>
2021-07-30 21:09:14 -0700

    Fix Bootloader source detection
    This fixes a long standing issue I've had with QMK DFU and it not exiting the bootloader properly.

--- Bootloaders/DFU/BootloaderDFU.c
+++ Bootloaders/DFU/BootloaderDFU.c
@@ -136,8 +136,8 @@ void Application_Jump_Check(void)
 		if (!(BootloaderAPI_ReadFuse(GET_HIGH_FUSE_BITS) & ~FUSE_BOOTRST))
 		{
 			/* If the reset source was not an external reset or the key is correct, clear it and jump to the application */
-			//if (!(MCUSR & (1 << EXTRF)) || (MagicBootKey == MAGIC_BOOT_KEY))
-			//  JumpToApplication = true;
+			if (!(MCUSR & (1 << EXTRF)) || (MagicBootKey == MAGIC_BOOT_KEY))
+			 JumpToApplication = true;
 
 			/* Clear reset source */
 			MCUSR &= ~(1 << EXTRF);
@@ -146,8 +146,8 @@ void Application_Jump_Check(void)
 		{
 			/* If the reset source was the bootloader and the key is correct, clear it and jump to the application;
 			 * this can happen in the HWBE fuse is set, and the HBE pin is low during the watchdog reset */
-			//if ((MCUSR & (1 << WDRF)) && (MagicBootKey == MAGIC_BOOT_KEY))
-			//	JumpToApplication = true;
+			if ((MCUSR & (1 << WDRF)) && (MagicBootKey == MAGIC_BOOT_KEY))
+				JumpToApplication = true;
 
 			/* Clear reset source */
 			MCUSR &= ~(1 << WDRF);
